c04b4147fb4e30e779097baf07d9165bd7d2fd1c
Geremy Giles <ggiles2@uccs.edu>
2025-10-26 12:32:07 -0600

    Create matrix

--- project3/project3.py
+++ project3/project3.py
@@ -6,11 +6,10 @@ Description: A program to encrypt and decrypt a user-inputted message using the 
 """
 
 #### Imports ####
+import math
 import numpy as np
 
 #### Definitions ####
-global file_path
-global file_contents
 rows = 0
 columns = 0
 selected_menu_option = -1
@@ -21,14 +20,14 @@ scalar = None
 
 #### Functions ####
 
+### Function for prompting user for file name
 def prompt_file_name():
 	# Request file name from user
 	print("Please input the name of the text file containing the message. (Omit the \".txt\")")
 
 	# Validate user input with loop
-	global file_path
 	file_path = ""
-	while file_path == "": # Check for valid input
+	while file_path == "":
 		try:
 			inputted_file_name = input("File Name: ").strip()
 			if inputted_file_name == "":
@@ -41,19 +40,72 @@ def prompt_file_name():
 	# Print file path
 	print("Inputted file path: " + file_path)
 
-def read_file():
-	global file_path
-	global file_contents
+	# Pass file path to read file function
+	read_file(file_path)
 
+
+
+### Function for reading file contents
+def read_file(path):
 	file_contents = ""
-	while file_contents == "": # Check for valid input
+	while file_contents == "": # Check for valid file path
+		# Try to open file with given path
 		try:
-			with open(file_path, 'r') as file:
+			with open(path, 'r') as file:
 				file_contents = file.read()
 				print("File Content: " + file_contents)
 		except FileNotFoundError:
-			print("File cannnot be found. Please check the file name and try again.\n")
+			print("File cannnot be found. Please check the file name and try again.\n") # Print error message
 			prompt_file_name()
+
+	# Pass file contents to create matrix function
+	create_matrix(file_contents)
+
+
+
+### Function for creating a 4xj matrix from file contents
+def create_matrix(contents):
+	# Create a matrix
+	matrix = []
+
+	# Calculate number of columns needed based on length of contents
+	print("Length of contents: " + str(len(contents)))
+	columns = math.ceil(float(len(contents)) / 4.0)
+	print("Number of columns needed: " + str(columns))
+
+	# Break contents into list
+	contents_list = list(contents.strip())
+	print("Contents List: " + str(contents_list))
+
+	for i in range(len(contents_list)):
+		contents_list[i] = ord(contents_list[i])
+
+	# Fill matrix by column, with 4 elements per column
+	for i in range(columns):
+		column = []
+		for i in range(4):
+			if len(contents_list) > 0:
+				column.append(contents_list.pop(0))
+			else:
+				column.append(0) # Padding character if contents run out
+		matrix.append(column)
+	
+	# Convert to numpy matrix
+	np_matrix = np.asmatrix(matrix)
+	print("NP Matrix: \n")
+	print(np_matrix)
+	print(np_matrix.T)
+		#	row = []
+		#	for j in range(columns):
+		#		while True:
+		#			try:
+		#				value = int(input(f"Enter value for position ({i+1},{j+1}): "))
+		#				row.append(value)
+		#				break
+		#			except ValueError:
+		#				print("Invalid input. Please enter an integer.")
+		#	matrix.append(row)
+		#return matrix
 
 #### Main Code ####
 
@@ -62,4 +114,3 @@ print("Part 1: Encrypting")
 
 # Prompt for file name
 prompt_file_name()
-read_file()
