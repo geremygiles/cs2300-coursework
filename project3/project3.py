c6ad16c9e09d7878df26c063cd224cc38170249f
Geremy Giles <ggiles2@uccs.edu>
2025-10-26 13:56:12 -0600

    Part 1 complete

--- project3/project3.py
+++ project3/project3.py
@@ -7,6 +7,7 @@ Description: A program to encrypt and decrypt a user-inputted message using the 
 
 #### Imports ####
 import math
+from tkinter import E
 import numpy as np
 
 #### Definitions ####
@@ -17,65 +18,70 @@ matrix1 = []
 matrix2 = []
 scalar = None
 
+matrix_a = [[1, 2, -2, 3], [-1, -3, -1, -3], [-1, -5, -2, -1], [1, 4, 2, 2]]
+np_matrix_a = np.asmatrix(matrix_a)
+
 
 #### Functions ####
 
 ### Function for prompting user for file name
-def prompt_file_name():
+### Returns the properly formatted file path
+def open_file():
 	# Request file name from user
 	print("Please input the name of the text file containing the message. (Omit the \".txt\")")
 
 	# Validate user input with loop
-	file_path = ""
-	while file_path == "":
-		try:
-			inputted_file_name = input("File Name: ").strip()
-			if inputted_file_name == "":
-				print("No input entered.") # Print error message
-			else:
-				file_path = inputted_file_name + ".txt"
-		except:
-			print("Invalid entry") # Print error message
-	
-	# Print file path
-	print("Inputted file path: " + file_path)
+	while True:
+		inputted_file_name = input("File Name: ").strip()
+		if inputted_file_name == "":
+			print("No input entered.\n") # Print error message
+		else:
+			file_path = inputted_file_name + ".txt" # Append the '.txt' extension
+			try:
+				with open(file_path, 'r') as file: # Attempt to open file
+					file_contents = read_file(file_path) # Read file with given path
+					return file_contents
+			except FileNotFoundError:
+				print(f"'{file_path}' cannnot be found. Please check the file name and try again.\n")
+				continue # Restart loop
+			except Exception as e:
+				print("The following error occurred: " + str(e) + "\nPlease try again.\n") # Print error message
+				continue # Restart loop
 
-	# Pass file path to read file function
-	read_file(file_path)
 
 
 
 ### Function for reading file contents
+### Takes the file path
+### Returns the contents of the file
 def read_file(path):
 	file_contents = ""
-	while file_contents == "": # Check for valid file path
-		# Try to open file with given path
-		try:
-			with open(path, 'r') as file:
-				file_contents = file.read()
-				print("File Content: " + file_contents)
-		except FileNotFoundError:
-			print("File cannnot be found. Please check the file name and try again.\n") # Print error message
-			prompt_file_name()
-
-	# Pass file contents to create matrix function
-	create_matrix(file_contents)
-
+	# Try to open file with given path
+	try:
+		with open(path, 'r') as file:
+			file_contents = file.read()
+			return file_contents
+	except FileNotFoundError:
+		print("File cannnot be found. Please check the file name and try again.\n") # Print error message
+		open_file()
+	except Exception as e:
+		print("The following error occurred: " + str(e) + "\nPlease try again.") # Print error message
+		open_file()
 
 
-### Function for creating a 4xj matrix from file contents
+
+### Function for creating a 4xj matrix of Unicode values from file contents
+### Takes the contents of the file
+### Returns the NumPy Matrix
 def create_matrix(contents):
 	# Create a matrix
 	matrix = []
 
 	# Calculate number of columns needed based on length of contents
-	print("Length of contents: " + str(len(contents)))
 	columns = math.ceil(float(len(contents)) / 4.0)
-	print("Number of columns needed: " + str(columns))
 
 	# Break contents into list
 	contents_list = list(contents.strip())
-	print("Contents List: " + str(contents_list))
 
 	for i in range(len(contents_list)):
 		contents_list[i] = ord(contents_list[i])
@@ -92,25 +98,20 @@ def create_matrix(contents):
 	
 	# Convert to numpy matrix
 	np_matrix = np.asmatrix(matrix)
-	print("NP Matrix: \n")
-	print(np_matrix)
-	print(np_matrix.T)
-		#	row = []
-		#	for j in range(columns):
-		#		while True:
-		#			try:
-		#				value = int(input(f"Enter value for position ({i+1},{j+1}): "))
-		#				row.append(value)
-		#				break
-		#			except ValueError:
-		#				print("Invalid input. Please enter an integer.")
-		#	matrix.append(row)
-		#return matrix
+
+	return np_matrix
 
 #### Main Code ####
 
 # Print title
 print("Part 1: Encrypting")
 
-# Prompt for file name
-prompt_file_name()
+
+file_contents = open_file() # Prompt for file name and read contents
+np_matrix_b = create_matrix(file_contents) # Create matrix from file contents
+print("\nMatrix B: \n")
+print(np_matrix_b.T) # Print the transpose of the matrix for better readability
+
+np_matrix_c = np_matrix_a.T * np_matrix_b.T # Create the encrypted matrix via matrix multiplication (transposed for correct dimensions))
+print("\nMatrix C: \n")
+print(np_matrix_c)
