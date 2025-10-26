59e8735b1d22f608c44a41e464c4b5c46b11c4b6
Geremy Giles <ggiles2@uccs.edu>
2025-10-26 15:09:08 -0600

    Part 2 complete

--- project3/project3.py
+++ project3/project3.py
@@ -7,19 +7,12 @@ Description: A program to encrypt and decrypt a user-inputted message using the 
 
 #### Imports ####
 import math
-from tkinter import E
 import numpy as np
 
 #### Definitions ####
-rows = 0
-columns = 0
-selected_menu_option = -1
-matrix1 = []
-matrix2 = []
-scalar = None
-
 matrix_a = [[1, 2, -2, 3], [-1, -3, -1, -3], [-1, -5, -2, -1], [1, 4, 2, 2]]
 np_matrix_a = np.asmatrix(matrix_a)
+np_matrix_a_inverse = np.round(np_matrix_a.I)
 
 
 #### Functions ####
@@ -71,20 +64,26 @@ def read_file(path):
 
 
 ### Function for creating a 4xj matrix of Unicode values from file contents
-### Takes the contents of the file
+### Takes the contents of the file, and the mode: 0 for encryption, 1 for decryption
 ### Returns the NumPy Matrix
-def create_matrix(contents):
+def create_matrix(contents, mode):
 	# Create a matrix
 	matrix = []
 
-	# Calculate number of columns needed based on length of contents
-	columns = math.ceil(float(len(contents)) / 4.0)
-
 	# Break contents into list
-	contents_list = list(contents.strip())
+	if mode == 0: contents_list = list(contents.strip())
+	elif mode == 1: contents_list = contents.strip().split(' ') # Split by spaces for decryption mode
+	
+	# Calculate number of columns needed based on length of contents
+	columns = math.ceil(float(len(contents_list)) / 4.0)
 
-	for i in range(len(contents_list)):
-		contents_list[i] = ord(contents_list[i])
+
+	if mode == 0: # Only convert to unicode for encryption mode
+		for i in range(len(contents_list)):
+			contents_list[i] = ord(contents_list[i])
+	if mode == 1: # Convert to intergers for decryption mode
+		for i in range(len(contents_list)):
+			contents_list[i] = int(contents_list[i])
 
 	# Fill matrix by column, with 4 elements per column
 	for i in range(columns):
@@ -98,20 +97,56 @@ def create_matrix(contents):
 	
 	# Convert to numpy matrix
 	np_matrix = np.asmatrix(matrix)
-
+	
 	return np_matrix
+
+
+
+### Function for converting decrypted matrix back to characters
+### Takes a matrix
+### Returns the decrypted message as a string
+def decrypt_message(matrix):
+	decrypted_message = ""
+	
+	matrix = matrix.T # Transpose matrix for easier iteration
+	
+	i = 0
+	while i < matrix.shape[0]: # Iterate through each row
+		j = 0
+		while j < matrix.shape[1]: # Iterate through each column
+			entry = int(matrix[i, j])
+			if entry != 0: # Ignore blank items
+				decrypted_message += chr(entry) # Add converted character to message
+			j += 1
+		i += 1
+
+	return decrypted_message
 
 #### Main Code ####
 
 # Print title
 print("Part 1: Encrypting")
 
-
 file_contents = open_file() # Prompt for file name and read contents
-np_matrix_b = create_matrix(file_contents) # Create matrix from file contents
+np_matrix_b = create_matrix(file_contents, 0) # Create matrix from file contents (encryption mode)
 print("\nMatrix B: \n")
 print(np_matrix_b.T) # Print the transpose of the matrix for better readability
 
 np_matrix_c = np_matrix_a.T * np_matrix_b.T # Create the encrypted matrix via matrix multiplication (transposed for correct dimensions))
 print("\nMatrix C: \n")
 print(np_matrix_c)
+
+
+
+# Print title
+print("\n\n\nPart 2: Decrypting")
+
+file_contents = open_file() # Prompt for file name and read contents
+np_matrix_c = create_matrix(file_contents, 1) # Create matrix from file contents (decryption mode)
+
+np_matrix_b = np_matrix_a_inverse.T * np_matrix_c.T # Create the encrypted matrix via matrix multiplication (transposed for correct dimensions))
+
+# Convert decrypted matrix back to characters
+message = decrypt_message(np_matrix_b)
+print("\nDecrypted Message: \n")
+print(message)
